1addc614f0c1c712bb617c3632c3cf515674545e
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2019-09-12 17:45:12 +0900

    init

---
+++ apps/client1/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'client1',
+  preset: '../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/apps/client1'
+};
