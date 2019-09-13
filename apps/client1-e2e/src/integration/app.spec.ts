c8465870341eda6bebb81d565dd27a957fb56d0f
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2019-09-13 11:05:33 +0900

    tutorial 6

--- apps/client1-e2e/src/integration/app.spec.ts
+++ apps/client1-e2e/src/integration/app.spec.ts
@@ -1,9 +1,11 @@
-import { getGreeting } from '../support/app.po';
+import { getAddTodoButton, getTodos } from '../support/app.po';
 
-describe('client1', () => {
+describe('TodoApps', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    getGreeting().contains('Welcome to client1!');
+  it('should display todos', () => {
+    getTodos().should(t => expect(t.length).equal(2));
+    getAddTodoButton().click();
+    getTodos().should(t => expect(t.length).equal(3));
   });
 });
